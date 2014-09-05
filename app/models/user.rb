c9a053c722188005bc8981e5965f48930429a221
Alexander J Marrs <ninjazord@gmail.com>
2014-09-04 23:46:40 -0600

    Add password_digest to users

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,11 +1,15 @@
 class User < ActiveRecord::Base
 
-  #has_secure_password
-
   has_many :tool_listings
   has_many :tools, through: :tool_listings
 
   has_many :skill_listings
   has_many :skills, through: :skill_listings
+
+  has_secure_password
+
+  validates :password, length: { minimum: 6 }
+  validates :password_confirmation, presence: true, length: { minimum: 6 }
+
 
 end
